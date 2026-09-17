ce63aa0f4690df8f751b4cb94155379521197af9
Grace Ministries <gracemediaweb@gmail.com>
2026-09-18 00:53:48 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -253,7 +253,7 @@ const Ministries = () => {
               className="aspect-[4/5] rounded-[100px] overflow-hidden"
             >
               <img 
-                src="/images/bpa.jpeg" 
+                src="/public/images/bpa.jpeg" 
                 alt="Ministry Work" 
                 className="w-full h-full object-cover"
                 referrerPolicy="no-referrer"
