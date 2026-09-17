63cf35199382abc435a2f128ee80846b3053c0ab
Grace Ministries <gracemediaweb@gmail.com>
2026-09-18 01:29:02 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -240,8 +240,8 @@ const Ministries = () => {
                 “Evangelism in Tamil villages followed by evangelism throughout India.”
               </div>
             </div>
-            <button className="mt-10 group flex items-center gap-2 text-sm font-bold uppercase tracking-widest border-b-2 border-primary pb-1">
-              About Our Mission <ChevronRight className="w-4 h-4 group-hover:translate-x-1 transition-transform" href="about"/>
+            <button href="#about"className="mt-10 group flex items-center gap-2 text-sm font-bold uppercase tracking-widest border-b-2 border-primary pb-1">
+              About Our Mission <ChevronRight className="w-4 h-4 group-hover:translate-x-1 transition-transform"/>
             </button>
           </motion.div>
 
